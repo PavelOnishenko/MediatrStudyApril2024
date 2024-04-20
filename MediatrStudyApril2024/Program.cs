e43451018ead6bc8239a46eeec3d21b1f6fba635
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2024-04-20 22:15:15 +0200

    Refactored.

--- MediatrStudyApril2024/Program.cs
+++ MediatrStudyApril2024/Program.cs
@@ -8,13 +8,20 @@ internal class Program
     // this program emulates system which stores data about energy efficiency measures applied to
     // power transmission lines and their operating modes and allows for assessment of impact of those measures
 
-    record Station(int Id, string Name, float EnergyLoss);
-    record Line(int StationId_1, int StationId_2);
+    record station(int id, string name, float energy_loss);
+    record line(int station_id_1, int station_id_2);
 
-    private static void Main(string[] args)
+    private static void Main()
     {
         var connectionString = LoadConnectionString();
-        if (connectionString is not null) SeedDbWithTestData(connectionString);
+        if (connectionString is null) return; 
+        var (stations, lines) = SeedDbWithTestData(connectionString);
+        var averageLossBeforeMeasures = GetAverageLoss(connectionString);
+        var stationToApplyMeasures = stations.Single(x => x.name == "Station A");
+        ApplyEfficiencyMeasures(connectionString, stationToApplyMeasures.id, stationToApplyMeasures.energy_loss - 0.5);
+        var averageLossAfterMeasures = GetAverageLoss(connectionString);
+        Console.WriteLine($"Average efficiency before was [{averageLossBeforeMeasures}], after became [{averageLossAfterMeasures}].");
+        Console.WriteLine($"Diff is [{averageLossAfterMeasures - averageLossBeforeMeasures}]]");
     }
 
     private static string? LoadConnectionString() => 
@@ -23,13 +30,14 @@ internal class Program
         .Build()
         .GetConnectionString("PostgreSQLConnection");
 
-    private static void SeedDbWithTestData(string connectionString)
+    private static (IEnumerable<station> stations, IEnumerable<line> lines) SeedDbWithTestData(string connectionString)
     {
         using var dbConnection = new NpgsqlConnection(connectionString);
         dbConnection.Open();
         ClearTables(dbConnection);
-        var stations = CreateStations(dbConnection);
-        CreateLines(dbConnection, stations.station1, stations.station2);
+        var (station1, station2) = CreateStations(dbConnection);
+        var lines = CreateLines(dbConnection, station1, station2);
+        return (new[] { station1, station2 }, lines);
     }
 
     private static void ClearTables(NpgsqlConnection dbConnection)
@@ -38,33 +46,36 @@ internal class Program
         dbConnection.Execute("DELETE FROM line;");
     }
 
-    private static (Station station1, Station station2) CreateStations(NpgsqlConnection dbConnection)
+    private static (station station1, station station2) CreateStations(NpgsqlConnection dbConnection)
     {
-        var station1 = new Station(0, "Station A", 3);
-        var station2 = new Station(0, "Station B", 5);
-        var stationInsertSql = "insert into station (name, energy_loss) values (@Name, @EnergyLoss) returning id;";
-        station1 = station1 with { Id = dbConnection.QuerySingle<int>(stationInsertSql, station1) };
-        station2 = station2 with { Id = dbConnection.QuerySingle<int>(stationInsertSql, station2) };
+        var station1 = new station(0, "Station A", 3);
+        var station2 = new station(0, "Station B", 5);
+        var stationInsertSql = "insert into station (name, energy_loss) values (@Name, @energy_loss) returning id;";
+        station1 = station1 with { id = dbConnection.QuerySingle<int>(stationInsertSql, station1) };
+        station2 = station2 with { id = dbConnection.QuerySingle<int>(stationInsertSql, station2) };
         return (station1, station2);
     }
 
-    private static void CreateLines(NpgsqlConnection dbConnection, Station station1, Station station2) => 
-        dbConnection.Execute("insert into Line (station_id_1, station_id_2) values (@StationId_1, @StationId_2);",
-            new Line(station1.Id, station2.Id));
+    private static line[] CreateLines(NpgsqlConnection dbConnection, station station1, station station2)
+    {
+        line param = new(station1.id, station2.id);
+        dbConnection.Execute("insert into line (station_id_1, station_id_2) values (@station_id_1, @station_id_2);", param);
+        return [param];
+    }
 
-    private static float GetEfficiency(string connectionString)
+    private static float GetAverageLoss(string connectionString)
     {
         using var dbConnection = new NpgsqlConnection(connectionString);
         dbConnection.Open();
-        var stations = dbConnection.Query<Station>("SELECT * FROM station;").ToArray();
-        return stations.Average(x => x.EnergyLoss);
+        var stations = dbConnection.Query<station>("SELECT * FROM station;").ToArray();
+        return stations.Average(x => x.energy_loss);
     }
 
     private static void ApplyEfficiencyMeasures(string connectionString, int stationId, double newEnergyLoss)
     {
         using var dbConnection = new NpgsqlConnection(connectionString);
         dbConnection.Open();
-        dbConnection.Execute("UPDATE station SET energyLoss = @newEnergyLoss WHERE id = @stationId;",
+        dbConnection.Execute("UPDATE station SET energy_loss = @newEnergyLoss WHERE id = @stationId;",
             new { stationId, newEnergyLoss });
         Console.WriteLine($"Updated Station {stationId} to have an energy loss of {newEnergyLoss}%.");
     }
